25bc348923e421b5321401435aec3a39826f4226
Dharmil Halpati <dharmilhalpatics12@gmai.com>
2024-10-14 17:38:54 +0530

    Remove all files from the repository

--- README.md
+++
@@ -1,2 +0,0 @@
-# CDC-Portal-SVNIT-
-Team Project for learning purpose
--- scripts/hashPassword.js
+++
@@ -1,8 +0,0 @@
-// scripts/hashPassword.js
-const bcrypt = require('bcryptjs');
-
-const password = 'testpassword';
-
-bcrypt.hash(password, 12, (err, hash) => {
-  if (err) throw err;
-});
