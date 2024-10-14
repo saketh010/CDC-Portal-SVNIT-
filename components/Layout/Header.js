0e707b444ff44572283f9dbc5c085093629954b2
Neem Sheth <neemsheth2003@gmail.com>
2024-10-14 17:31:53 +0530

    Route to Profile

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -64,7 +64,7 @@ export default function Header(){
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
           <li>
-            <a className="justify-between">
+            <a href='/profile' className="justify-between">
               Profile
               <span className="badge"><FaRegUserCircle size={20} /></span>
             </a>
